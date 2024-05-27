ad0238f0a25520f40e00b6c5ae1fd7a8372c4392
LSB30 <lukinhaasantana777@gmail.com>
2024-05-27 14:54:11 -0300

    � init

---
+++ src/main/java/belato/lucas/agregadordeinvestimentos/controller/CreateUserDto.java
@@ -0,0 +1,4 @@
+package belato.lucas.agregadordeinvestimentos.controller;
+
+public record CreateUserDto(String username, String email, String password) {
+}
---
+++ src/main/java/belato/lucas/agregadordeinvestimentos/service/UserService.java
@@ -0,0 +1,30 @@
+package belato.lucas.agregadordeinvestimentos.service;
+
+import belato.lucas.agregadordeinvestimentos.Repository.UserRepository;
+import belato.lucas.agregadordeinvestimentos.controller.CreateUserDto;
+import belato.lucas.agregadordeinvestimentos.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UUID createUser(CreateUserDto createUserDto) {
+
+        System.out.println(createUserDto);
+
+        var entity = new User(UUID.randomUUID(),createUserDto.username(),createUserDto.email(), createUserDto.password(),Instant.now(),null);
+
+        var userSaved = userRepository.save(entity);
+
+        return userSaved.getUserId();
+    }
+}
