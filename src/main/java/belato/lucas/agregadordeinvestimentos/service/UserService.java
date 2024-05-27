4e6b52582065ced0e43980737eed6866398d3aaa
LSB30 <lukinhaasantana777@gmail.com>
2024-05-27 15:31:27 -0300

    feat:⚡ findUserById

--- src/main/java/belato/lucas/agregadordeinvestimentos/service/UserService.java
+++ src/main/java/belato/lucas/agregadordeinvestimentos/service/UserService.java
@@ -6,6 +6,7 @@ import belato.lucas.agregadordeinvestimentos.entity.User;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -26,5 +27,9 @@ public class UserService {
         var userSaved = userRepository.save(entity);
 
         return userSaved.getUserId();
+    }
+
+    public Optional<User> getUserById(String userId) {
+       return userRepository.findById(UUID.fromString(userId));
     }
 }
